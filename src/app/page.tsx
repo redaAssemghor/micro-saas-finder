2fced10bea20be68799ae4ed75d029395baf3da0
redaAssemghor <assemghor.reda@gmail.com>
2024-06-08 23:03:27 +0100

    type fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
     try {
       const response = await generateIdeas({ niche });
       if (response !== null) {
-        setIdeas(response);
       } else {
         setIdeas(["No response received"]);
       }
